57ab0acf3f27f1aa14d1fa7e338b1123d2970dc1
ChialoLee <52813314+ChialoLee@users.noreply.github.com>
2023-01-12 11:34:59 +0800

    Update Server.jl

--- src/Server.jl
+++ src/Server.jl
@@ -3,18 +3,16 @@ Pkg.add("Genie")
 using Genie, Genie.Requests, Genie.Renderer.Json
 
 route("/") do
-  (:message => "Hi there! 12345") |> json
-end
-original_folder = pwd()
-route("/falra/dist/", method = POST) do
-  @show jsonpayload()
-  @show rawpayload()
-  cd("github/bohachu/add")
-  output=readchomp(`julia main.jl 2 3`)
-  @show output
-  cd(original_folder)
-  (:message => "Hi there! 67890") |> json
+  "Welcome to 🐤Falra!"
 end
 
+original_folder = pwd()
+route("/falra/dist/", method = POST) do
+  dic = jsonpayload()
+  cd("github/$(dic["user_name"])/$(dic["project_name"])")
+  output=readchomp(`$(dic["lst_command"]) $(dic["script_filename"]) $(dic["lst_parameters"])`)
+  cd(original_folder)
+  output
+end
 
 up(8080,"0.0.0.0",async=true,verbose=false)
