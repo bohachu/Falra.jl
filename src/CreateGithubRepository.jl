a692d1e49c8bddbcf7b90dd573eae9d8f15dbe42
chialolee <carole1727@gmail.com>
2023-07-27 11:45:01 -0400

    ok

--- src/CreateGithubRepository.jl
+++ src/CreateGithubRepository.jl
@@ -1,44 +1,28 @@
 using HTTP
 using JSON
+function create_github_repository(token::String, user_name, repo_name::String, private::Bool=false)
+    try
+        url = "https://api.github.com/user/repos"
+        headers = Dict(
+            "Authorization" => "token $token",
+            "Accept" => "application/vnd.github.v3+json",
+        )
 
-function check_github_repository_exists(token::String, repo_name::String)
-    url = "https://api.github.com/repos/<your_username>/$repo_name"
+        data = Dict(
+            "name" => repo_name,
+            "private" => private,
+            "auto_init" => true,
+            "default_branch" => "main"
+        )
 
-    headers = Dict(
-        "Authorization" => "token $token",
-        "Accept" => "application/vnd.github.v3+json",
-    )
+        response = HTTP.request("POST", url, headers, JSON.json(data))
 
-    response = HTTP.request("GET", url, headers)
-
-    return response.status == 200
-end
-
-function create_github_repository(token::String, repo_name::String, private::Bool=false)
-    if check_github_repository_exists(token, repo_name)
-        println("The repository $repo_name already exists.")
-        return
-    end
-
-    url = "https://api.github.com/user/repos"
-
-    headers = Dict(
-        "Authorization" => "token $token",
-        "Accept" => "application/vnd.github.v3+json",
-    )
-
-    data = Dict(
-        "name" => repo_name,
-        "private" => private,
-        "auto_init" => true,
-        "default_branch" => "main"
-    )
-
-    response = HTTP.request("POST", url, headers, JSON.json(data))
-
-    if response.status == 201
-        println("Successfully created the repository $repo_name")
-    else
-        println("Failed to create the repository: $(String(response.body))")
+        if response.status == 201
+            println("Successfully created the repository $repo_name")
+        else
+            println("Failed to create the repository: $(String(response.body))")
+        end
+    catch e
+        println("exception:",e)
     end
 end
