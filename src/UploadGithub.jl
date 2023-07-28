33124f2f8e0cc5c7aac34328af437f2941d988d6
chialolee <carole1727@gmail.com>
2023-07-28 01:31:32 -0400

    ok

--- src/UploadGithub.jl
+++ src/UploadGithub.jl
@@ -8,6 +8,7 @@ function upload_github(user_directory, token::String, repo::String, files::Dict{
     end
     cd(repo_name)
     run(`git checkout $(branch)`)
+    run(`cp -r ../Falra.jl .`)
     for (source_path, target_path) in files
         target_dir = dirname(target_path)
         if !isdir(target_dir)
